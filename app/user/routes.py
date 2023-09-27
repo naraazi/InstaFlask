b9ba8627ca7fbf45e8decad34d03e701d27a0924
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2023-09-27 17:26:19 -0300

    Form de 'Sign Up' adicionado

---
+++ app/user/routes.py
@@ -0,0 +1,17 @@
+from app.extensions import db
+from app.user import user
+from app.user.models import User
+from app.user.forms import SignupForm
+from flask import render_template
+
+
+@user.route('/signup', methods=['get', 'post'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(name=form.name.data, username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return "<h1>User Added</h1>"
+    return render_template('signup.html', form=form)
